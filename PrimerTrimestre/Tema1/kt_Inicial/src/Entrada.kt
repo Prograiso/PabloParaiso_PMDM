dd0f6f874b6a53c46f8d15577a9afbf87e8089a3
Prograiso <pparaisosanchez@gmail.com>
2025-10-14 17:00:09 +0200

    Revert "cambio a tema2 y android studio primer proyecto"
    This reverts commit 2c709293170655aeb3de17e6222b77f00cc2e178.

--- PrimerTrimestre/Tema1/kt_Inicial/src/Entrada.kt
+++ PrimerTrimestre/Tema1/kt_Inicial/src/Entrada.kt
@@ -1,8 +1,6 @@
-var sumaLambda: ((Int,Int) -> Int)? = {a,b -> a+b}
-
 fun main (args:Array<String>)  {
 
-    /*val DNI = "12345678B" //inmutable
+    val DNI = "12345678B" //inmutable
     var nombre = "Pablo" //mutable
     var edad : Int = 34
     lateinit var correo : String
@@ -22,12 +20,8 @@ fun main (args:Array<String>)  {
     println("cuantas veces quieres saludar?")
     var veces = readln().toInt()
 
-    saludar (nombre=nombre,veces=veces)*/
-
-    println("El resultado es ${sumaLambda?.invoke(2,3) ?: "la función es nula"}")
-
-    sentenciaWhen(3)
-    sentenciaFor()
+    saludar (nombre=nombre,veces=veces)
+    //sumaLambda(2,3)
 
 }
 
@@ -37,38 +31,6 @@ fun saludar (nombre: String?=null,veces:Int? = null) {
     }
 }
 
-fun sentenciaWhen(opcion:Int) {
-    when (opcion) {
-        1 -> println("entrando por ejecución de caso 1")
 
-        in 2..5 -> {
-            println("entrando por ejecución de caso rango 2 a 5")
-        }
-
-        else -> {
-            println("opción no contemplada")
-        }
-    }
-} //revisar git de clase añade fun sumalamdba
-
-fun sentenciaFor() {
-   /* for (item in 0..10 step 2){
-        println(item)
-    }
-    for (item in 10 downTo 0 step 2){ //devuelve el rango inverso
-        println(item)
-    }
-    for (item in (10..20) step 8){ //devuelve el rango de 10 a 20 de 8 en 8
-        println(item)
-    }
-    */
-
-    (20..40).forEach { it : Int ->
-        println(it)
-    }
-
-    (30..40).forEachIndexed{index, item -> if (index %2 ==0)
-        println("el elemento en posicion par $index son $item ")  }
-}
-
+//var sumaLambda: (Int,Int) -> Int =
 
