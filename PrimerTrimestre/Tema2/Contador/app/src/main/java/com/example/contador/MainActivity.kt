40ff947b222dcf9b45d8968b7c41f7f186edb5be
Prograiso <pparaisosanchez@gmail.com>
2025-10-21 17:53:28 +0200

    biding e inicio de app contador

--- PrimerTrimestre/Tema2/Contador/app/src/main/java/com/example/contador/MainActivity.kt
+++ PrimerTrimestre/Tema2/Contador/app/src/main/java/com/example/contador/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.contador
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.contador.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) //  valores grafico/logico, esta linea los junta
+    }
+
+    override fun onResume() {
+        super.onResume()
+        acciones()
+    }
+
+    private fun acciones() {
+        binding.botonIncremento.setOnClickListener {
+            Snackbar.make(it,
+                "pulsado incremento",
+                Snackbar.LENGTH_LONG).show()
+
+        } 
     }
 }
